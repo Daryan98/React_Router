403532eaf3655abaac8aa516ee49ac42758a59c8
Daryan Kamalifar <32463453+Daryan98@users.noreply.github.com>
2018-09-18 11:21:53 +0200

    pushing the finnished files

---
+++ src/components/Lost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Lost = () => {
+    return ( 
+        <div>
+            <h1 style={{color: "white"}}>You're lost! Get back...get...back...now!!</h1>
+            <img style={{
+                paddingTop: "50px"
+            }} src="/img/biohazard.png"/>
+        </div>
+     );
+}
+ 
+export default Lost;
